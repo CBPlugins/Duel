60c1fbaff0ab23d634ec4c6b54abd75d854578ff
CBPlugins <cbplugins@gmail.com>
2015-11-27 20:20:08 +0100

    Added the main class

---
+++ src/main/java/com/github/cbplugins/duel/Duel.java
@@ -0,0 +1,18 @@
+package com.github.cbplugins.duel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by CBPlugins on 27.11.2015.
+ */
+public class Duel extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
